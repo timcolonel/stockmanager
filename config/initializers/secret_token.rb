4de1c153818c970b423a9fe9cad4db5e21115bf1
Timothee Guerin <timothee.guerin@outlook.com>
2013-09-29 18:54:43 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stockmanager::Application.config.secret_key_base = '5ed208b39719c9a36ed7ab5b1cb569c477946fdc53a7d43c578ec087a3c15ac0493d5faa8f8910ee8dec361fc9df6757dcc1572f21dd1d6a6bedc7175a3df810'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stockmanager::Application.config.session_store :cookie_store, key: '_stockmanager_session'
